52cf88fb5d2633a2aea7d5cd967e1b119063d181
jerem <llanta.jeremy@gmail.com>
2021-03-02 18:37:53 +0100

    start header component

---
+++ components/header.vue
@@ -0,0 +1,20 @@
+<template>
+  <a-menu v-model="current" mode="horizontal">
+    <a-menu-item key="list">
+      <a-icon type="unordered-list" />List Stars</a-menu-item
+    >
+    <a-menu-item key="favorites"> <a-icon type="star" />Favorites</a-menu-item>
+  </a-menu>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
+
+<style scoped>
+.ant-menu {
+  margin-bottom: 32px;
+}
+</style>
